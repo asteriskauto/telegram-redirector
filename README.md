12fc3a7dbe88bb205393a909a3b98e57e0b13d54
nobleman <ispanteleev@gmail.com>
2017-04-05 21:42:10 +0300

    minor fix

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ pip install -r requirements.txt
 3. Invite @botname to group
 4. Type /start to get your chat id
 5. Alter chat_id variable according to your group
-6. Now you can run redirector py with argument, wich will be postet to binded chat.
+6. Now you can run redirector py with argument, wich will be posted to binded chat.
--- redirector.py
+++ redirector.py
@@ -4,9 +4,10 @@
 import telebot
 import sys
 
-token = '353egervsdv6DVdVS7dv6sDVSvsd7v6s57vsdvs6d7v5' # Вводим свой телеграм API токен
-group_id = -123456789 # Вводим id группы, куда надо слать сообщения (int с отрицательным значением)
+token = '353egervsdv6DVdVS7dv6sDVSvsd7v6s57vsdvs6d7v5'  # Вводим свой телеграм API токен
+group_id = -123456789  # Вводим id группы, куда надо слать сообщения (int с отрицательным значением)
 bot = telebot.TeleBot(token, skip_pending=True)
+
 
 # Ловим команду старта при старте без аргументов (первый старт)
 @bot.message_handler(func=lambda message: True, commands=['start'])
@@ -17,10 +18,11 @@ def start(message):
     print message.chat.id
     sys.exit()
 
+
 # Если был передан один аргумент, то возвращаем в чат сообщение
 if len(sys.argv) == 2:
     number = str(sys.argv[1])
-    bot.send_message(group_id, "Вызова с номера " + number)
+    bot.send_message(group_id, "Вызов с номера " + number)
 
 # Если аргумента нет, то считаем первым стартом и запускаем в режиме полинга
 if len(sys.argv) == 1:
