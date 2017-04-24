cfb8c60ead2b1c5ffa5c3ec7cefcf46c63bb4af6
Mikhail <mnkomov@gmail.com>
2017-04-24 10:57:39 +0000

    Update redirector.py

--- redirector.py
+++ redirector.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python2.7
+#!/usr/local/bin/python2.7
 # -*- coding: utf-8 -*-
 
 import telebot
 import sys
 
-token = '353egervsdv6DVdVS7dv6sDVSvsd7v6s57vsdvs6d7v5'  # Вводим свой телеграм API токен
-group_id = -123456789  # Вводим id группы, куда надо слать сообщения (int с отрицательным значением)
+token = '273781229:AAF2WoqkKrHSEJAtWbyDemlWo09X9VBTGaU'  # Вводим свой телеграм API токен
+group_id = -193777042  # Вводим id группы, куда надо слать сообщения (int с отрицательным значением)
 bot = telebot.TeleBot(token, skip_pending=True)
 
 
@@ -19,11 +19,12 @@ def start(message):
     sys.exit()
 
 
-# Если был передан один аргумент, то возвращаем в чат сообщение
-if len(sys.argv) == 2:
-    number = str(sys.argv[1])
-    bot.send_message(group_id, "Вызов с номера " + number)
-
+# Если были переданы три аргумента, то возвращаем в чат сообщение
+if len(sys.argv) == 4:
+    callerid = str(sys.argv[1])
+    exten = str(sys.argv[2])
+    redirectnum = str(sys.argv[3])
+    bot.send_message(group_id, "Вызов с номера " + callerid + "\nна номер " + exten + "\nпереадресован на номер " + redirectnum)
 # Если аргумента нет, то считаем первым стартом и запускаем в режиме полинга
 if len(sys.argv) == 1:
     bot.polling(none_stop=True)
